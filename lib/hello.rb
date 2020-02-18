481dc1e3e469a1db230a049dc899097b758ba9a7
Melissa Sperry <sperry.mel@gmail.com>
2020-02-18 04:56:35 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,12 +1,4 @@
-def map(array)
-    i = 0
-    while array[i] do
-    yield(array, i)
-    end
-map([1, 2, 3], i) do |x|
-while x[i] do
-neg_arr = []
- y = array[i] * -1
-neg_arr << y
-i += 1
+def "#{hello_t}" do
+["Tim", "Tom", "Jim"].each do |name|
+  puts "Hi, #{name}"
 end
